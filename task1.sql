06668e9c95e94d59283c3d2d57c63a42383303dd
Priyangshu Sekhar Laha <priyangshulaha07@gmail.com>
2025-06-23 18:13:50 +0530

    Add files via upload

---
+++ task1.sql
@@ -0,0 +1,30 @@
+create database LIB;
+show databases;
+use LIB;
+CREATE TABLE Members (
+    MemberID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    JoinDate DATE
+);
+
+CREATE TABLE Books (
+    BookID INT PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    ISBN VARCHAR(20),
+    Publisher VARCHAR(100),
+    Quantity INT
+);
+
+CREATE TABLE IssuedBooks (
+    IssueID INT PRIMARY KEY,
+    BookID INT,
+    MemberID INT,
+    IssueDate DATE,
+    ReturnDate DATE,
+    FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+desc Members;
+drop table Members;
